5fd2d7ca1ec56a959e8c2d674913e8081ffda5ee
Noah Harris <jnharris@boomtownroi.com>
2019-12-24 22:08:59 -0500

    Read and write files from the command line
    main is no longer a skeleton for testing and now handles file i/o

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from document import Document
 
 def get_args():
     parser = argparse.ArgumentParser(description = "Generate markdown docs for REST")
+    parser.add_argument("-o", "--out", default = "out.md", type = str)
     parser.add_argument("file", nargs = 1, default = "")
     return parser.parse_args()
 
 def main():
     args = get_args()
     parsed_document = load_document(args.file[0]).parse_variables().parse_globals()
-    print(json.dumps(parsed_document.routes))
+    with open(args.out, "w") as out:
+        out.write(parsed_document.rendered)
 
 def load_document(name):
     return Document(open(name).read())
